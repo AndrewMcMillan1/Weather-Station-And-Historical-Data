ec07c7a01325f2e6c9f68a81d761ef5fe31412fc
AndrewMcMillan1 <77256915+AndrewMcMillan1@users.noreply.github.com>
2021-04-23 18:17:51 -0500

    Update main.py
    Added main function, sensor function, and timer logic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # !/usr/bin/env python
+
 import math
 import time
 import grovepi
@@ -14,34 +15,48 @@ dht_sensor_type = 0
 # List to hold tuples of sensor data
 data = []
 
-while True:
+def readData():
 
-    try:
+    # insert reading into tuple
+    [temp, humidity] = grovepi.dht(dht_sensor_port, dht_sensor_type)
 
-        [temp, humidity] = grovepi.dht(dht_sensor_port, dht_sensor_type)
+    # make sure reading is a number
+    if math.isnan(temp) == False and math.isnan(humidity) == False:
+        # celsius to fahrenheit
+        fahrenheit = ((temp * 9) / 5.0) + 32
+        print("temp = %.02f F humidity =%.02f%%" % (fahrenheit, humidity))
+        t = fahrenheit
+        h = humidity
 
-        if math.isnan(temp) == False and math.isnan(humidity) == False:
-            # celsius to fahrenheit
-            fahrenheit = ((temp * 9) / 5.0) + 32
-            print("temp = %.02f F humidity =%.02f%%" % (fahrenheit, humidity))
-            t = fahrenheit
-            h = humidity
+        # append tuple to list
+        data.append([t, h])
 
-            data.append([t, h])
+    else:
+        print("No data")
 
-        else:
-            print("No data")
 
-    except IOError:
-        print("Error")
+# main method
+if __name__ == '__main__':
 
-    except KeyboardInterrupt as e:
-        print(str(e))
+    while True:
 
-        break
+        try:
+            while curr_clock < '23:59:00':
+                readData()
+                sleep(4)
+                curr_time = time.localtime()
+                curr_clock = time.strftime("%H:%M:%S", curr_time)
 
-    #  3 minutes between readings
-    sleep(3)
+            sleep(60)
+
+        except IOError:
+            print("Error")
+
+        except KeyboardInterrupt as e:
+            print(str(e))
+
+            break
+
 
 
 
